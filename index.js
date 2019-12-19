13bee402f5dc656267307aa7280a7374ae7adcb5
global-domain-9186 <jasonscottkutner@gmail.com>
2019-12-19 19:42:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -40,5 +40,5 @@
 function takeANumber(katzDeli, name) {
   katzDeli.push(`${name}`);
     katzDeli;
-      return console.log(`Welcome, ${name}. You are number ${katzDeli.length} in line.`);
+      console.log(`Welcome, ${name}. You are number ${katzDeli.length} in line.`);
 }
