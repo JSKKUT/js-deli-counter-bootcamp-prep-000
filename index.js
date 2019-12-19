a786ceed39c2881365fe1f35171f258671658da2
global-domain-9186 <jasonscottkutner@gmail.com>
2019-12-19 19:33:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 //  1) deli takeANumber adds a person to the line: ReferenceError: takeANumber is not defined
 //  2) deli takeANumber appends the person the end of the line if there are already people on it: ReferenceError: takeANumber is not defined
-//  3) deli takeANumber properly handles multiple people being added: ReferenceError: takeANumber is not defined
+                    //  3) deli takeANumber properly handles multiple people being added: ReferenceError: takeANumber is not defined
 //  4) deli nowServing returns the line is empty when no one is on line: ReferenceError: nowServing is not defined
 //  5) deli nowServing returns an announcement about the person it is serving, and shifts the line: ReferenceError: nowServing is not defined
 //  6) deli currentLine(line) returns "The line is currently empty." if no one isin line: ReferenceError: currentLine is not defined
 //  7) deli currentLine(line) says who is in line when there are people waiting: ReferenceError: currentLine is not defined
 
-
-// return console.log(`Welcome, ${name}. You are number ${XXX} in line.`)
 
 
 // describe('deli', () => {
@@ -40,5 +38,7 @@
 //   });
 
 function takeANumber(katzDeli, name) {
-  katzDeli.push(`${name}`)
+  katzDeli.push(`${name}`);
 }
+
+return console.log(`Welcome, ${name}. You are number ${XXX} in line.`)
