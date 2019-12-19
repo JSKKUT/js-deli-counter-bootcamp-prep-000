1574b0ade08665bc7e186fe2d4f25eca3e1065c6
global-domain-9186 <jasonscottkutner@gmail.com>
2019-12-19 20:01:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -30,6 +30,7 @@ function takeANumber(katzDeli, name) {
 function nowServing(katzDeli) {
   if (katzDeli.length === 0) {
     return "There is nobody waiting to be served!"
+  } else {
     katzDeli.shift();
 }
 
