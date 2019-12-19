8fc21d8e5f90d7f9133c16624c755cdd56591db7
global-domain-9186 <jasonscottkutner@gmail.com>
2019-12-19 21:52:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
                     //  1) 
-                    //  2) deli takeANumber appends the person the end of the line if there are already people on it: ReferenceError: takeANumber is not defined
+                    //  2) 
                     //  3) deli takeANumber properly handles multiple people being added: ReferenceError: takeANumber is not defined
 function takeANumber(katzDeli, name) {
   katzDeli.push(name);
