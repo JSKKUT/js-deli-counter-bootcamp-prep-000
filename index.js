8b077a122aea75d9fee7d509bd40dce76967da41
global-domain-9186 <jasonscottkutner@gmail.com>
2019-12-19 20:48:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -29,13 +29,14 @@ function nowServing(katzDeli) {
 function currentLine(katzDeli) {
   var i = 0
   var list = ["The line is currently:"]
-  var n = i + 1
+  var n = 1
   if (katzDeli.length === 0) {
     return "The line is currently empty."
   } else {
       while (i < katzDeli.length) {
         list.push(`${n}. ${katzDeli[i]}`);
         i++;
+        n++;
       }
         return list;
       }
