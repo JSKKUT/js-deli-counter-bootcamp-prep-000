18ddf1af18cdf1648a139d9420e0abf4ef6801c2
global-domain-9186 <jasonscottkutner@gmail.com>
2019-12-19 19:46:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-//  1) deli takeANumber adds a person to the line: ReferenceError: takeANumber is not defined
+                    //  1) deli takeANumber adds a person to the line: ReferenceError: takeANumber is not defined
 //  2) deli takeANumber appends the person the end of the line if there are already people on it: ReferenceError: takeANumber is not defined
                     //  3) deli takeANumber properly handles multiple people being added: ReferenceError: takeANumber is not defined
 //  4) deli nowServing returns the line is empty when no one is on line: ReferenceError: nowServing is not defined
