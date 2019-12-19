3a30afa4e6cc8efc5eebff9a2b49543dc53a5918
global-domain-9186 <jasonscottkutner@gmail.com>
2019-12-19 19:25:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,6 +6,35 @@
 //  6) deli currentLine(line) returns "The line is currently empty." if no one isin line: ReferenceError: currentLine is not defined
 //  7) deli currentLine(line) says who is in line when there are people waiting: ReferenceError: currentLine is not defined
 
+describe('deli', () => {
+  describe('takeANumber', () => {
+    var katzDeli;
+    var otherDeli;
+
+    beforeEach(() => {
+      katzDeli = [];
+      otherDeli = ["Steven", "Blake", "Avi"];
+    });
+
+    it('adds a person to the line', () => {
+      expect(takeANumber(katzDeli, 'Ada')).toEqual("Welcome, Ada. You are number 1 in line.");
+      expect(katzDeli).toEqual(['Ada']);
+    });
+
+    it('appends the person the end of the line if there are already people on it', () => {
+      expect(takeANumber(otherDeli, 'Grace')).toEqual("Welcome, Grace. You are number 4 in line.");
+      expect(otherDeli).toEqual(["Steven", "Blake", "Avi", "Grace"]);
+    });
+
+    it("properly handles multiple people being added", () => {
+      takeANumber(katzDeli, 'Ada');
+      takeANumber(katzDeli, 'Grace');
+      takeANumber(katzDeli, 'Kent');
+
+      expect(katzDeli).toEqual(["Ada", "Grace", "Kent"]);
+    });
+  });
+
 function takeANumber(line, name) {
   
   return console.log(`Welcome, ${name}. You are number ${XXX} in line.`)
