a14a4cb982a7dc4c21a964a7331c0b87b0a44663
global-domain-9186 <jasonscottkutner@gmail.com>
2019-12-19 20:29:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ function currentLine(katzDeli) {
   if (katzDeli.length === 0) {
     return "The line is currently empty."
   } else {
-      
+      list.push()
   return `The line is currently: 1. NAME, 2. NAME`
   }
 }
